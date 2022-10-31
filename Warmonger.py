6d0e34ccf17ac3ebdc2ce9edd84bbea5fd0a9545
ozgurates <71925636+ozgurates@users.noreply.github.com>
2022-10-31 22:57:34 +0300

    Added the playing function

--- Warmonger.py
+++ Warmonger.py
@@ -19,13 +19,13 @@ class Faction:
         return self.numOfUnits * self.attackPoint
 
     def ReceiveAttack(self,attack):
-        self.numOfUnits -= attack
+        self.numOfUnits -= int(attack)
 
     def PurchaseWeapons(self,newWeapon):
-        self.attackPoint += newWeapon
+        self.attackPoint += int(newWeapon)
 
     def PurchaseArmors(self,newArmor):
-        self.healthPoint += newArmor
+        self.healthPoint += int(newArmor)
 
 
     def Print(self):
@@ -58,17 +58,17 @@ class Orcs(Faction):
     def PerformAttack(self):
         if(self.firstEnemy.isAlive and self.secondEnemy.isAlive):
             if(self.firstEnemy.__class__.__name__ == "Dwarves"):
-                self.firstEnemy.ReceiveAttack(super().PerformAttack()*0.7,self.firstEnemy)
-                self.secondEnemy.ReceiveAttack(super().PerformAttack()*0.3,self.secondEnemy)
+                self.firstEnemy.ReceiveAttack(super().PerformAttack()*0.7,self)
+                self.secondEnemy.ReceiveAttack(super().PerformAttack()*0.3,self)
             elif(self.firstEnemy.__class__.__name__ == "Elves"):
-                self.firstEnemy.ReceiveAttack(super().PerformAttack()*0.3,self.firstEnemy)
-                self.secondEnemy.ReceiveAttack(super().PerformAttack()*0.7,self.secondEnemy)
+                self.firstEnemy.ReceiveAttack(super().PerformAttack()*0.3,self)
+                self.secondEnemy.ReceiveAttack(super().PerformAttack()*0.7,self)
             else:
                 print('Something has gone wrong!')
         elif(self.firsEnemy.isAlive and not self.secondEnemy.isAlive):
-            self.firstEnemy.ReceiveAttack(super().PerformAttack(),self.firstEnemy)
+            self.firstEnemy.ReceiveAttack(super().PerformAttack(),self)
         elif(not self.firstEnemy.isAlive and self.secondEnemy.isAlive):
-            self.secondEnemy.ReceiveAttack(super().PerformAttack(),self.secondEnemy)
+            self.secondEnemy.ReceiveAttack(super().PerformAttack(),self)
         else:
             print('All enemies are dead! ')
 
@@ -80,7 +80,7 @@ class Orcs(Faction):
         elif(attacker.__class__.__name__ == "Elves"):
             super().ReceiveAttack((attack*(0.75))/(self.healthPoint))
         else:
-            print('Something has gone wrong!')
+            print('Something has gone wrong')
 
     def PurchaseWeapons(self,weapon):
         super().PurchaseWeapons(2*weapon)
@@ -103,12 +103,12 @@ class Dwarves(Faction):
 
     def PerformAttack(self):
         if(self.firstEnemy.isAlive and self.secondEnemy.isAlive):
-            self.firstEnemy.ReceiveAttack((super().PerformAttack())/2,self.firstEnemy)
-            self.secondEnemy.ReceiveAttack((super().PerformAttack())/2,self.secondEnemy)
+            self.firstEnemy.ReceiveAttack(super().PerformAttack()/2,self)
+            self.secondEnemy.ReceiveAttack(super().PerformAttack()/2,self)
         elif(self.firsEnemy.isAlive and not self.secondEnemy.isAlive):
-            self.firstEnemy.ReceiveAttack(super().PerformAttack(),self.firstEnemy)
+            self.firstEnemy.ReceiveAttack(super().PerformAttack(),self)
         elif(not self.firstEnemy.isAlive and self.secondEnemy.isAlive):
-            self.secondEnemy.ReceiveAttack(super().PerformAttack(),self.secondEnemy)
+            self.secondEnemy.ReceiveAttack(super().PerformAttack(),self)
         else:
             print('All enemies are dead! ')
     
@@ -116,7 +116,7 @@ class Dwarves(Faction):
         try:
             super().ReceiveAttack((attack)/(self.healthPoint))
         except:
-            print('Something has gone wrong!')
+            print('Something has gone wrong')
 
 
     def PurchaseWeapons(self,weapon):
@@ -141,8 +141,8 @@ class Elves(Faction):
     
     def PerformAttack(self):
         if(self.firstEnemy.isAlive and self.secondEnemy.isAlive):
-            self.firstEnemy.ReceiveAttack((super().PerformAttack())*0.6,self.firstEnemy)#(1.5)*(0.4)
-            self.secondEnemy.ReceiveAttack((super().PerformAttack())*0.6,self.secondEnemy)
+            self.firstEnemy.ReceiveAttack((super().PerformAttack())*0.6,self)#(1.5)*(0.4)
+            self.secondEnemy.ReceiveAttack((super().PerformAttack())*0.6,self)
 
             #if(self.firstEnemy.__class__.__name__ == "Dwarves"):
             #    self.firstEnemy.ReceiveAttack((super().PerformAttack())*0.6)#(1.5)*(0.4)
@@ -152,14 +152,14 @@ class Elves(Faction):
             #    self.secondEnemy.ReceiveAttack((super().PerformAttack())*0.6)
         elif(self.firsEnemy.isAlive and not self.secondEnemy.isAlive):
             if(self.firstEnemy.__class__.__name__ == "Dwarves"):
-                self.firstEnemy.ReceiveAttack((super().PerformAttack())*1.5,self.firstEnemy)
+                self.firstEnemy.ReceiveAttack((super().PerformAttack())*1.5,self)
             else:
-                self.firstEnemy.ReceiveAttack(super().PerformAttack(),self.firstEnemy)
+                self.firstEnemy.ReceiveAttack(super().PerformAttack(),self)
         elif(not self.firstEnemy.isAlive and self.secondEnemy.isAlive):
             if(self.secondEnemy.__class__.__name__ == "Dwarves"):
-                self.secondEnemy.ReceiveAttack((super().PerformAttack())*1.5,self.secondEnemy)
+                self.secondEnemy.ReceiveAttack((super().PerformAttack())*1.5,self)
             else:
-                self.secondEnemy.ReceiveAttack(super().PerformAttack(),self.secondEnemy)
+                self.secondEnemy.ReceiveAttack(super().PerformAttack(),self)
         else:
             print('All enemies are dead! ')
     
@@ -167,7 +167,6 @@ class Elves(Faction):
         if(attacker.__class__.__name__ == "Orcs"):
             super().ReceiveAttack((attack*(1.25))/(self.healthPoint))
         else:
-            pass
             super().ReceiveAttack((attack*(0.75))/(self.healthPoint))
 
 
@@ -205,10 +204,10 @@ class Merchant:
     
     def SellWeapons(self,wp,fact):
         if(fact.isAlive):
-            if(wp > self.weaponPoint):
+            if(int(wp) > self.weaponPoint):
                 print('You try to sell more weapons than you have in possession.')
             else:
-                self.revenue += fact.PurchaseWeapons(wp)
+                self.revenue += fact.PurchaseWeapons(int(wp))
                 print('Weapons sold!')
         else:
             print('The faction you want to sell weapons is dead!')
@@ -216,13 +215,16 @@ class Merchant:
 
     def SellArmors(self,ap,fact):
         if(fact.isAlive):
-            if(ap > self.armorPoint):
+            if(int(ap) > self.armorPoint):
                 print('You try to sell more armors than you have in possession.')
+                return False
             else:
-                self.revenue += fact.PurchaseArmors(ap)
+                self.revenue += fact.PurchaseArmors(int(ap))
                 print('Armors sold!')
+                return True
         else:
-            print('The faction you want to sell armors is dead!')
+            print('The faction you want to sell armors is destroyed!')
+            return False
 
 
     def EndTurn(self):
@@ -242,12 +244,108 @@ dwarves.AssignEnemies(orcs,elves)
 elves.AssignEnemies(orcs,dwarves)
 
 
-dwarves.Print()
-elves.Print()
-orcs.PerformAttack()
-dwarves.Print()
-elves.Print()
+def StartOver():
+    orcs = Orcs('orcs',40,20,10,5)
+    dwarves = Dwarves('dwarves',60,10,20,10)
+    elves = Elves('elves',30,20,20,5)
+    merchant = Merchant(20,20)
+    
+    orcs.AssignEnemies(dwarves,elves)
+    dwarves.AssignEnemies(orcs,elves)
+    elves.AssignEnemies(orcs,dwarves)
 
-orcs.Print()
-merchant.SellArmors(10,orcs)
-orcs.Print()
+
+
+def StartGame():
+    orcs.Print()
+    dwarves.Print()
+    elves.Print()
+
+    orcs.PerformAttack()
+    elves.PerformAttack()
+    dwarves.PerformAttack()
+
+    print('\nAfter the first attack phase, faction information are as follows: \n')
+    orcs.Print()
+    dwarves.Print()
+    elves.Print()
+
+
+    while(merchant.armorPoint>0 and merchant.weaponPoint>0):
+        str2 = """Merchant selling phase! Select a faction to sell a weapon to! 
+        1)Orcs
+        2)Dwarves
+        3)Elves
+        """
+
+        str3 = """Merchant selling phase! Select a faction to sell a armor to! 
+        1)Orcs
+        2)Dwarves
+        3)Elves
+        """
+
+        val2 = input(str2)
+        val3 = input(str3)
+
+        if(val2 == '1'):
+            wpn = input('Input how many weapons you want to sell! \n')
+            merchant.SellWeapons(wpn,orcs)
+        
+
+        elif(val2 == '2'):
+            wpn = input('Input how many weapons you want to sell! \n')
+            merchant.SellWeapons(wpn,dwarves)
+        elif(val2 == '3'):
+            wpn = input('Input how many weapons you want to sell! \n')
+            merchant.SellWeapons(wpn,elves)
+        else:
+            print('Input is not recognized! \n')
+
+
+        if(val3 == '1'):
+            arm = input('Input how many armors you want to sell! \n')
+            merchant.SellArmors(wpn,orcs)
+        elif(val3 == '2'):
+            arm = input('Input how many armors you want to sell! \n')
+            merchant.SellArmors(arm,dwarves)
+        elif(val3 == '3'):
+            arm = input('Input how many armors you want to sell! \n')
+            merchant.SellArmors(arm,elves)
+        else:
+            print('Input is not recognized! \n')
+
+
+        inp = input('Press 1 if you want to end the selling phase! \n')
+        if(inp == '1'):
+            break
+
+    str4 = """
+    1)End day
+    2)End currently played game and start a new one
+    3)Quit game
+    """
+
+    end = input(str4)
+
+    if(end == '1'):
+        orcs.EndTurn()
+        dwarves.EndTurn()
+        elves.EndTurn()
+        merchant.EndTurn()
+        StartGame()
+    elif(end == '2'):
+        
+        StartOver()
+        StartGame()
+        
+    elif(end == '3'):
+        return 0
+    else:
+        print('Input is not recognized! \n')
+
+
+
+
+
+
+StartGame()
